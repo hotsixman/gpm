4eecb4dfcd90df4efa85b42869cc69f0faf7fa30
hotsixman <hotsixman05@gmail.com>
2026-04-08 01:03:23 +0900

    fix: github build

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-GOOS=linux GOARCH=amd64 go build -o dist/geep-linux
-GOOS=darwin GOARCH=arm64 go build -o dist/geep-mac-arm
-GOOS=windows GOARCH=amd64 go build -o dist/geep-windows-x64.exe
-GOOS=windows GOARCH=386 go build -o dist/geep-windows-x32.exe
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o dist/geep-linux
+CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build -o dist/geep-mac-arm
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o dist/geep-windows-x64.exe
+CGO_ENABLED=0 GOOS=windows GOARCH=386 go build -o dist/geep-windows-x32.exe
